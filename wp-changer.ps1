ce85e81231750ed67b107e7a088dc449cbafc3de
sparklester <sparklester1@gmail.com>
2021-02-28 00:06:26 +0100

    Add generic path to the script

--- wp-changer.ps1
+++ wp-changer.ps1
@@ -1,12 +1,12 @@
 
-#  _______________________________/\\\______________________        
-#   ______________________________\/\\\______________________       
-#    ______________________________\/\\\______________________      
-#     _____/\\\\\\\\_____/\\\\\\\\__\/\\\____________/\\\\\____     
-#      ___/\\\//////____/\\\/////\\\_\/\\\\\\\\\____/\\\///\\\__    
-#       __/\\\__________/\\\\\\\\\\\__\/\\\////\\\__/\\\__\//\\\_   
-#        _\//\\\________\//\\///////___\/\\\__\/\\\_\//\\\__/\\\__  
-#         __\///\\\\\\\\__\//\\\\\\\\\\_\/\\\\\\\\\___\///\\\\\/___ 
+#  _______________________________/\\\______________________
+#   ______________________________\/\\\______________________
+#    ______________________________\/\\\______________________
+#     _____/\\\\\\\\_____/\\\\\\\\__\/\\\____________/\\\\\____
+#      ___/\\\//////____/\\\/////\\\_\/\\\\\\\\\____/\\\///\\\__
+#       __/\\\__________/\\\\\\\\\\\__\/\\\////\\\__/\\\__\//\\\_
+#        _\//\\\________\//\\///////___\/\\\__\/\\\_\//\\\__/\\\__
+#         __\///\\\\\\\\__\//\\\\\\\\\\_\/\\\\\\\\\___\///\\\\\/___
 #          ____\////////____\//////////__\/////////______\/////_____
 
 #------------------------------------------------------------------------#
@@ -17,23 +17,23 @@
 #------------------------------------------------------------------------#
 
 #Modify Path to the picture accordingly to reflect your infrastructure
-$imgPath="C:\Users\DavidCebotari\AppData\Roaming\Scripts\wallpaper-changer\bliss.jpg"
-$code = @' 
-using System.Runtime.InteropServices; 
-namespace Win32{ 
-    
-     public class Wallpaper{ 
-        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
-         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
-         
-         public static void SetWallpaper(string thePath){ 
-            SystemParametersInfo(20,0,thePath,3); 
+$imgPath="C:\Users\NameSurname\AppData\Roaming\Scripts\wallpaper-changer\bliss.jpg"
+$code = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+
+     public class Wallpaper{
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+
+         public static void SetWallpaper(string thePath){
+            SystemParametersInfo(20,0,thePath,3);
          }
     }
- } 
+ }
 '@
 
-add-type $code 
+add-type $code
 
-#Apply the Change on the system 
+#Apply the Change on the system
 [Win32.Wallpaper]::SetWallpaper($imgPath)
